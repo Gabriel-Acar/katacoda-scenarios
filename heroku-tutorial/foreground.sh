0b6a910ba5586168aecbde3c44b661b017114ea2
Gabriel-Acar <98453966+Gabriel-Acar@users.noreply.github.com>
2022-04-18 19:21:27 +0200

    Update foreground.sh

--- heroku-tutorial/foreground.sh
+++ heroku-tutorial/foreground.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Please wait while Heroku CLI is installed.";
+echo "Please wait while Heroku CLI is installed."
 
-snap install heroku --classic;
+snap install heroku --classic
 
-echo "Successfully installed Heroku CLI.";
+echo "Successfully installed Heroku CLI."
 
-echo "Configuring github alias..";
+echo "Configuring github alias.."
 
-git config --global user.email "demo@demo.com";
-git config --global user.name "Demo";
+git config --global user.email "demo@demo.com"
+git config --global user.name "Demo"
 
-echo "git aliases configured!";
+echo "git aliases configured!"
